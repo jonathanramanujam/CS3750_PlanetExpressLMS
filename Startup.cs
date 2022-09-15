9751704c224845b46340468a811f90443c14e5fb
jonathanramanujam <jonathanramanujam@mail.weber.edu>
2022-09-15 14:07:12 -0600

    One more change

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace CS3750_PlanetExpressLMS
             services.AddRazorPages();
 
             services.AddDbContext<CS3750_PlanetExpressLMSContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("CS3750_A1Context")));
+                    options.UseSqlServer(Configuration.GetConnectionString("CS3750_PlanetExpressLMSContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
