bec30b880f07ddc53586acd4d93878381479896d
Nathan Blair <98774772+Nathan-Blair@users.noreply.github.com>
2022-09-20 15:10:00 -0600

    Navigation Links
    Removed header from _layout and added headers to each individual page. Added profile, registration and classes links. Classes and registration only appear when user is in proper role.

--- Pages/Registration.cshtml.cs
+++ Pages/Registration.cshtml.cs
@@ -1,5 +1,8 @@
+using CS3750_PlanetExpressLMS.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace CS3750_A1.Pages
 {
