c825a86b9c34a43f0094fb21451fc3e3ecf0b8f8
Nathan Blair <98774772+Nathan-Blair@users.noreply.github.com>
2022-09-20 18:00:59 -0600

    Nav update
    Changed credential to user

--- Pages/Classes.cshtml.cs
+++ Pages/Classes.cshtml.cs
@@ -8,8 +8,29 @@ namespace CS3750_A1.Pages
 {
     public class ClassesModel : PageModel
     {
-        public void OnGet()
+        private readonly CS3750_PlanetExpressLMS.Data.CS3750_PlanetExpressLMSContext _context;
+
+        public ClassesModel(CS3750_PlanetExpressLMS.Data.CS3750_PlanetExpressLMSContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            // If no id was passed, return not found
+            if (id == null) { return NotFound(); }
+
+            // Look up the user based on the id
+            User = await _context.User.FirstOrDefaultAsync(c => c.ID == id);
+
+            // If the user does not exist, return not found
+            if (User == null) { return NotFound(); }
+
+            // Otherwise, return the page
+            return Page();
         }
     }
 }
