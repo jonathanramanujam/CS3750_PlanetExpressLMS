9b01fc8ee499f4b4f60d0e3d1b2c4327f2dbf785
Nathan Blair <98774772+Nathan-Blair@users.noreply.github.com>
2022-09-23 20:01:33 -0600

    Database fix
    Added new field and changed data type

---
+++ Migrations/20220924015733_NewCourseEnrollment.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS3750_PlanetExpressLMS.Migrations
+{
+    public partial class NewCourseEnrollment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Enrollment",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Days",
+                table: "Course",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Enrollment");
+
+            migrationBuilder.DropColumn(
+                name: "Days",
+                table: "Course");
+        }
+    }
+}
