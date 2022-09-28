852d30dce786b7b908b3615e6079907e20b9b4c5
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-09-28 00:04:48 -0600

    Implemented CRUD operations for Enrollment class via SQLEnrollmentRepository

--- Data/SQLEnrollmentRepository.cs
+++ Data/SQLEnrollmentRepository.cs
@@ -5,29 +5,55 @@ namespace CS3750_PlanetExpressLMS.Data
 {
     public class SQLEnrollmentRepository : IEnrollmentRepository
     {
+        public readonly CS3750_PlanetExpressLMSContext context;
+        public SQLEnrollmentRepository(CS3750_PlanetExpressLMSContext context)
+        {
+            this.context = context;
+        }
+
         public Enrollment Add(Enrollment newEnrollment)
         {
+            context.Enrollment.Add(newEnrollment);
+            context.SaveChanges();
             return newEnrollment;
         }
 
         public Enrollment Delete(int id)
         {
-            throw new System.NotImplementedException();
+            Enrollment en = context.Enrollment.Find(id);
+            if(en != null)
+            {
+                context.Enrollment.Remove(en);
+                context.SaveChanges();
+            }
+            return en;
         }
 
         public IEnumerable<Enrollment> GetAllEnrollments()
         {
-            throw new System.NotImplementedException();
+            return context.Enrollment;
         }
 
         public Enrollment GetEnrollment(int id)
         {
-            throw new System.NotImplementedException();
+            return context.Enrollment.Find(id);
         }
 
         public Enrollment Update(Enrollment updatedEnrollment)
         {
-            throw new System.NotImplementedException();
+            var en = context.Enrollment.Attach(updatedEnrollment);
+            //Right now, this updates the ENTIRE Course object.
+            //If you don't want that to happen, remove the below statement:
+            en.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            //Then add statements here like, for example:
+            //context.Entry(updatedEnrollment).Property("UserID").IsModified = true;
+            //And ONLY those attributes will be updated.
+            context.SaveChanges();
+            //Then, replace the return statement with this to 'reset' the context and get good values:
+            /*context.Entry(updatedEnrollment).State = Microsoft.EntityFrameworkCore.EntityState.Detached;
+              var enrollment = context.Enrollment.Find(updatedEnrollment.ID);
+              return course;*/
+            return updatedEnrollment;
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,7 @@ namespace CS3750_PlanetExpressLMS
             services.AddRazorPages();
             services.AddScoped<IUserRepository, SQLUserRepository>();
             services.AddScoped<ICourseRepository, SQLCourseRepository>();
+            services.AddScoped<IEnrollmentRepository, SQLEnrollmentRepository>();
             services.AddDbContext<CS3750_PlanetExpressLMSContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("CS3750_PlanetExpressLMSContext")));
         }
