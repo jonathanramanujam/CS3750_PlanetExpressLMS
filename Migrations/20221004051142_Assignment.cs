965f11221f4b25117f90eb9ebac16c16d60a7646
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-10-03 23:12:13 -0600

    Added Assignment model.

---
+++ Migrations/20221004051142_Assignment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS3750_PlanetExpressLMS.Migrations
+{
+    public partial class Assignment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Assignment",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    SubmissionType = table.Column<string>(maxLength: 15, nullable: false),
+                    PointsPossible = table.Column<int>(nullable: false),
+                    OpenDateTime = table.Column<DateTime>(nullable: false),
+                    CloseDateTime = table.Column<DateTime>(nullable: false),
+                    CourseID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assignment", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Assignment");
+        }
+    }
+}
---
+++ Models/Assignment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS3750_PlanetExpressLMS.Models
+{
+    public class Assignment
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Assignment Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Submission Type")]
+        [StringLength(15)]
+        public string SubmissionType { get; set; }
+        [Required]
+        [Display(Name = "Points Possible")]
+        public int PointsPossible { get; set; }
+        [Required]
+        [Display(Name = "Open Date / Time")]
+        public DateTime OpenDateTime { get; set; }
+        [Required]
+        [Display(Name = "Close Date / Time")]
+        public DateTime CloseDateTime { get; set; }
+        //FK to Course
+        [ForeignKey("CourseID")]
+        public int CourseID { get; set; }
+
+    }
+}
