f5a806a7b28ac5bb9d765edaacf0c7efa97b3cd5
Nathan Blair <nathanblair@mail.weber.edu>
2022-10-04 10:05:42 -0600

    PaymentUpdate
    Payment and Invoice tables

---
+++ Migrations/20221004160315_Payment.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS3750_PlanetExpressLMS.Migrations
+{
+    public partial class Payment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Pages/Account/Account.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS3750_PlanetExpressLMS.Pages.Account
+{
+    public class AccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
