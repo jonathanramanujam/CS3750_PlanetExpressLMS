47862659359a1d0d4073ffabc84cbe5f18776022
Nathan Blair <nathanblair@mail.weber.edu>
2022-10-04 10:09:22 -0600

    Payment Update
    Added Payment and Invoice tables

---
+++ Migrations/20221004160759_Invoice.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS3750_PlanetExpressLMS.Migrations
+{
+    public partial class Invoice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoice",
+                columns: table => new
+                {
+                    InvoiceId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID = table.Column<int>(nullable: false),
+                    FullBalance = table.Column<decimal>(nullable: false),
+                    AmountPaid = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoice", x => x.InvoiceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    PaymentID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(maxLength: 20, nullable: false),
+                    CardNumber = table.Column<string>(maxLength: 16, nullable: false),
+                    ExpDate = table.Column<DateTime>(nullable: false),
+                    Cvv = table.Column<string>(maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.PaymentID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoice");
+
+            migrationBuilder.DropTable(
+                name: "Payment");
+        }
+    }
+}
