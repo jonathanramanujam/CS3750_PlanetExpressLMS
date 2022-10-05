c84a66f0d2ddac330f795831df8ff505da33d546
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-10-05 17:47:46 -0600

    Created initial chart test page

---
+++ Pages/ChartTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS3750_PlanetExpressLMS.Pages
+{
+    public class ChartTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
