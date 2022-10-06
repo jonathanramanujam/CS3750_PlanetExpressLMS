607921f1932505b93c4abe0131eb023f404a4348
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-10-05 18:58:15 -0600

    Added chart that gets info from a Model.

--- Pages/ChartTest.cshtml.cs
+++ Pages/ChartTest.cshtml.cs
@@ -1,12 +1,26 @@
+using CS3750_PlanetExpressLMS.Data;
+using CS3750_PlanetExpressLMS.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CS3750_PlanetExpressLMS.Pages
 {
     public class ChartTestModel : PageModel
     {
+        IUserRepository userRepository;
+        
+        public List<User> Users;
+
+        public ChartTestModel(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
         public void OnGet()
         {
+            Users = userRepository.GetAllUsers().ToList();
         }
     }
 }
