0ac8ed86b0d9e827430021aa41e6010543f12ebd
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-10-05 20:45:41 -0600

    Updated Assignment class and assignment creation with issue resolutions
    Added description and range limit for points possible. Added description stuff to the course detail page.

--- Models/Assignment.cs
+++ Models/Assignment.cs
@@ -18,6 +18,7 @@ namespace CS3750_PlanetExpressLMS.Models
         [StringLength(4)]
         public string SubmissionType { get; set; }
         [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Points cannot be negative.")]
         [Display(Name = "Points Possible")]
         public int PointsPossible { get; set; }
         [Required]
@@ -26,6 +27,8 @@ namespace CS3750_PlanetExpressLMS.Models
         [Required]
         [Display(Name = "Close Date / Time")]
         public DateTime CloseDateTime { get; set; }
+
+        public string Description { get; set; }
         //FK to Course
         [ForeignKey("CourseID")]
         public int CourseID { get; set; }
