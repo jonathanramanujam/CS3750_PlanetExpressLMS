8e791f8b1dfc5b8b67920365a8b65e0049bb843a
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-10-10 21:51:29 -0600

    Initial SubmitAssignment page creation; fixed some student/instructor permissions issues on CourseDetail

---
+++ Pages/SubmitAssignment.cshtml.cs
@@ -0,0 +1,27 @@
+using CS3750_PlanetExpressLMS.Data;
+using CS3750_PlanetExpressLMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS3750_PlanetExpressLMS.Pages
+{
+    public class SubmitAssignmentModel : PageModel
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IAssignmentRepository assignmentRepository;
+
+        public SubmitAssignmentModel(IUserRepository userRepository, IAssignmentRepository assignmentRepository)
+        {
+            this.userRepository = userRepository;
+            this.assignmentRepository = assignmentRepository;
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+
+        public void OnGet(int userId, int assignmentId)
+        {
+            User = userRepository.GetUser(userId);
+        }
+    }
+}
