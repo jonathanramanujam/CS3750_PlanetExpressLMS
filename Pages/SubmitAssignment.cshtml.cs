de449886dae61cee3001838ce5ada23560f55999
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-10-11 20:01:45 -0600

    Added text box functionality that creates a .txt file. Also organized things into functions a bit more.

--- Pages/SubmitAssignment.cshtml.cs
+++ Pages/SubmitAssignment.cshtml.cs
@@ -23,10 +23,8 @@ namespace CS3750_PlanetExpressLMS.Pages
             _environment = environment;
         }
 
-        [BindProperty]
         public User User { get; set; }
 
-        [BindProperty]
         public Assignment Assignment { get; set; }
 
         [BindProperty]
@@ -35,6 +33,9 @@ namespace CS3750_PlanetExpressLMS.Pages
         [BindProperty]
         public IFormFile Upload { get; set; }
 
+        [BindProperty]
+        public string SubmissionString { get; set; }
+
         public IActionResult OnGet(int userId, int assignmentId)
         {
             User = userRepository.GetUser(userId);
@@ -42,37 +43,54 @@ namespace CS3750_PlanetExpressLMS.Pages
             return Page();
         }
 
-        //Create a new submission entry in the database
         public IActionResult OnPost(int userId, int assignmentId)
         {
             User = userRepository.GetUser(userId);
             Assignment = assignmentRepository.GetAssignment(assignmentId);
-            Submission.AssignmentID = assignmentId;
-            Submission.UserID = userId;
-            
-            submissionRepository.Add(Submission);
-            return Page();
-        }
+            string filePath = "";
 
-
-        public IActionResult OnPostFileUpload(int userId, int assignmentId)
-        {
-            User = userRepository.GetUser(userId);
-            Assignment = assignmentRepository.GetAssignment(assignmentId);
-            //Add a file upload to the wwwroot folder
-            var fileName = GetFileName(Upload, userId);
-            var file = Path.Combine(_environment.ContentRootPath, "wwwroot", "submissions", fileName);
-            using (var fileStream = new FileStream(file, FileMode.Create))
+            if (Assignment.SubmissionType.Equals("FILE"))
             {
-                Upload.CopyTo(fileStream);
+                //Add a file upload to the wwwroot/submissions folder
+                filePath = FileUpload(userId);
             }
+            else //Generate a new txt file in wwwroot/submissions
+            {
+                filePath = TextBoxUpload(userId, assignmentId);
+            }
+
             //Create new submission object
             Submission.AssignmentID = assignmentId;
             Submission.UserID = userId;
-            Submission.Path = file;
+            Submission.Path = filePath;
             submissionRepository.Add(Submission);
 
             return Page();
+        }
+
+        public string FileUpload(int userId)
+        {
+            var fileName = GetFileName(Upload, userId);
+            var filePath = Path.Combine(_environment.ContentRootPath, "wwwroot", "submissions", fileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                Upload.CopyTo(fileStream);
+            }
+            return filePath;
+        }
+
+        public string TextBoxUpload(int userId, int assignmentId)
+        {
+            //Create file name and path
+            var fileName = GetTextBoxFileName(userId, assignmentId);
+            var filePath = Path.Combine(_environment.ContentRootPath, "wwwroot", "submissions", fileName);
+            //Generate file in the appropriate folder
+            FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(SubmissionString);
+            writer.Close();
+
+            return filePath;
         }
 
         //Append user first and last name to file name, then extension
@@ -82,6 +100,14 @@ namespace CS3750_PlanetExpressLMS.Pages
             var name = Path.GetFileNameWithoutExtension(Upload.FileName);
             var ext = Path.GetExtension(Upload.FileName);
             return name + "_" + User.FirstName + User.LastName + ext;
+        }
+
+        //Generate a file name for a text box submission
+        public string GetTextBoxFileName(int userId, int assignmentId)
+        {
+            User = userRepository.GetUser(userId);
+            Assignment = assignmentRepository.GetAssignment(assignmentId);
+            return Assignment.Name + "_" + User.FirstName + User.LastName + ".txt";
         }
     }
 }
