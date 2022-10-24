a7bedcd3a243ba7de9cfc2bee9c5c2ba64265f66
Nathan Blair <nathanblair@mail.weber.edu>
2022-10-24 11:05:20 -0600

    notification model
    Added notification model

---
+++ Data/SQLNotificationRepository.cs
@@ -0,0 +1,41 @@
+﻿using CS3750_PlanetExpressLMS.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CS3750_PlanetExpressLMS.Data
+{
+    public class SQLNotificationRepository //: INotificationRepository
+    {
+/*        public readonly CS3750_PlanetExpressLMSContext context;
+        public SQLNotificationRepository(CS3750_PlanetExpressLMSContext context)
+        {
+            this.context = context;
+        }
+        public Notification Add(Notification newNotification)
+        {
+            context.Notification.Add(newNotification);
+            context.SaveChanges();
+            return newNotification;
+        }
+        public IEnumerable<Notification> GetAllNotifications()
+        {
+            return context.Notification;
+        }
+        public List<Notification> GetNotifications(int id)
+        {
+            var userNotification = GetAllNotification();
+            userNotification = userNotification.Where(c => c.ID == id);
+            return userNotification.ToList<Notification>();
+        }
+        public Notification Delete(int id)
+        {
+            Notification no = context.Notification.Find(id);
+            if (no != null)
+            {
+                context.Notification.Remove(no);
+                context.SaveChanges();
+            }
+            return no;
+        }*/
+    }
+}
