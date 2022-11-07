d4fec920f24bb0ddc75c271e1bb697a46f0ec497
bhtaylor97 <brookstaylor@mail.weber.edu>
2022-11-07 15:50:07 -0700

    Organized CourseDetail and fixed several bugs

--- Pages/CourseDetail.cshtml.cs
+++ Pages/CourseDetail.cshtml.cs
@@ -15,14 +15,12 @@ namespace CS3750_PlanetExpressLMS.Pages
         private readonly IAssignmentRepository assignmentRepository;
         private readonly ISubmissionRepository submissionRepository;
         private readonly IEnrollmentRepository enrollmentRepository;
-        private IWebHostEnvironment _environment;
 
-        public CourseDetailModel(IAssignmentRepository assignmentRepository, ISubmissionRepository submissionRepository, IEnrollmentRepository enrollmentRepository, IWebHostEnvironment environment)
+        public CourseDetailModel(IAssignmentRepository assignmentRepository, ISubmissionRepository submissionRepository, IEnrollmentRepository enrollmentRepository)
         {
             this.assignmentRepository = assignmentRepository;
             this.submissionRepository = submissionRepository;
             this.enrollmentRepository = enrollmentRepository;
-            _environment = environment;
         }
 
         [BindProperty]
@@ -62,6 +60,7 @@ namespace CS3750_PlanetExpressLMS.Pages
         public string? letterGrade { get; set; }
 
 
+
         public async Task<IActionResult> OnGetAsync (int courseID)
         {
             // Access the current session
@@ -88,22 +87,25 @@ namespace CS3750_PlanetExpressLMS.Pages
 
             if (course == null) { return NotFound(); }
 
-            //Get all enrollments for this course
+            //Initialize all lists
             courseEnrollments = enrollmentRepository.GetEnrollmentsByCourse(courseID);
-
-            // Check for existing assignments for this course
             courseAssignments = assignmentRepository.GetAssignmentsByCourse(courseID).ToList();
-            //Initialize submission list for this course
             courseSubmissions = new Submission[courseAssignments.Count()];
+            assignmentHasSubmission = new bool[courseAssignments.Count()];
+            submissions = submissionRepository.GetStudentSubmissions(user.ID).ToList();
+
+            //Get enrollment
+            foreach (Enrollment e in courseEnrollments)
+            {
+                if (user.ID == e.UserID)
+                {
+                    enrollment = e;
+                }
+            }
 
             //If user is a student, and the course has assignments, check for submissions
             if (!user.IsInstructor && courseAssignments.Count() != 0)
             {
-                submissions = submissionRepository.GetStudentSubmissions(user.ID).ToList();
-
-                //Track, for each assignment, if the student has submitted it
-                assignmentHasSubmission = new bool[courseAssignments.Count()];
-
                 for (int i = 0; i < courseAssignments.Count(); i++)
                 {
                     foreach (Submission submission in submissions)
@@ -144,65 +146,10 @@ namespace CS3750_PlanetExpressLMS.Pages
                 {
                     percentGrade = Decimal.Round(((decimal)(totalPointsEarned / (decimal?)totalPointsPossible) * 100), 2);
 
-                    if (percentGrade >= 94)
-                    {
-                        letterGrade = "A";
-                    }
-                    else if (percentGrade >= 90)
-                    {
-                        letterGrade = "A-";
-                    }
-                    else if (percentGrade >= 87)
-                    {
-                        letterGrade = "B+";
-                    }
-                    else if (percentGrade >= 84)
-                    {
-                        letterGrade = "B";
-                    }
-                    else if (percentGrade >= 80)
-                    {
-                        letterGrade = "B-";
-                    }
-                    else if (percentGrade >= 77)
-                    {
-                        letterGrade = "C+";
-                    }
-                    else if (percentGrade >= 74)
-                    {
-                        letterGrade = "C";
-                    }
-                    else if (percentGrade >= 70)
-                    {
-                        letterGrade = "C-";
-                    }
-                    else if (percentGrade >= 67)
-                    {
-                        letterGrade = "D+";
-                    }
-                    else if (percentGrade >= 64)
-                    {
-                        letterGrade = "D";
-                    }
-                    else if (percentGrade >= 60)
-                    {
-                        letterGrade = "D-";
-                    }
-                    else
-                    {
-                        letterGrade = "E";
-                    }
+                    letterGrade = GetLetterGrade(percentGrade);
                 }
 
-                    //Save the percent grade in the current student's Enrollment object
-                    foreach (Enrollment e in courseEnrollments)
-                    {
-                        if (user.ID == e.UserID)
-                        {
-                            enrollment = e;
-                        }
-                    }
-
+                //Save the percent grade in the current student's Enrollment object
                 if (totalPointsPossible > 0 && enrollment.CumulativeGrade != (decimal)percentGrade)
                 {
                     enrollment.CumulativeGrade = (decimal)percentGrade;
@@ -212,6 +159,8 @@ namespace CS3750_PlanetExpressLMS.Pages
             }
             return Page();
         }
+
+
 
         public IActionResult OnPost(int courseId)
         {
@@ -247,6 +196,9 @@ namespace CS3750_PlanetExpressLMS.Pages
             return Page();
         }
 
+
+
+
         public IActionResult OnPostDelete(int assignmentId, int courseId)
         {
             // Access the current session
@@ -264,6 +216,62 @@ namespace CS3750_PlanetExpressLMS.Pages
             assignmentRepository.Delete(assignmentId);
 
             return Redirect("/CourseDetail/" + courseId);
+        }
+
+
+
+        public string GetLetterGrade(decimal? percentGrade)
+        {
+            if (percentGrade >= 94)
+            {
+                letterGrade = "A";
+            }
+            else if (percentGrade >= 90)
+            {
+                letterGrade = "A-";
+            }
+            else if (percentGrade >= 87)
+            {
+                letterGrade = "B+";
+            }
+            else if (percentGrade >= 84)
+            {
+                letterGrade = "B";
+            }
+            else if (percentGrade >= 80)
+            {
+                letterGrade = "B-";
+            }
+            else if (percentGrade >= 77)
+            {
+                letterGrade = "C+";
+            }
+            else if (percentGrade >= 74)
+            {
+                letterGrade = "C";
+            }
+            else if (percentGrade >= 70)
+            {
+                letterGrade = "C-";
+            }
+            else if (percentGrade >= 67)
+            {
+                letterGrade = "D+";
+            }
+            else if (percentGrade >= 64)
+            {
+                letterGrade = "D";
+            }
+            else if (percentGrade >= 60)
+            {
+                letterGrade = "D-";
+            }
+            else
+            {
+                letterGrade = "E";
+            }
+
+            return letterGrade;
         }
     }
 }
